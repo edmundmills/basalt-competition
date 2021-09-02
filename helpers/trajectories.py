f5402a526ea44cf8e7df99196caf67a7022414f4
Edmund Mills <edmund.martin.mills@gmail.com>
2021-09-01 17:35:10 -0700

    Add trajectory viewer

--- helpers/trajectories.py
+++ helpers/trajectories.py
@@ -7,6 +7,10 @@ import shutil
 import numpy as np
 import torch as th
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.widgets import Slider
+
 
 class Trajectory:
     def __init__(self):
@@ -61,6 +65,9 @@ class Trajectory:
                         for frame_idx in frame_indices])
         return frames
 
+    def view(self):
+        viewer = TrajectoryViewer(self).view()
+
 
 class TrajectoryGenerator:
     def __init__(self, env, agent):
@@ -79,3 +86,88 @@ class TrajectoryGenerator:
             obs, _, done, _ = self.env.step(action)
             trajectory.done = done
         return trajectory
+
+
+class TrajectoryViewer:
+    def __init__(self, trajectory):
+        self.trajectory = trajectory
+        self.current_step = 0
+        self.manual_control = False
+
+    def view(self):
+        def play():
+            if len(self.trajectory) > 1:
+                slider.set_val(self.current_frame)
+                slider.on_changed(update_slider)
+                fig.canvas.mpl_connect('button_press_event', on_click)
+                ani = animation.FuncAnimation(
+                    fig, update_plot, animation_data_gen, interval=2, blit=True)
+            fig.canvas.mpl_connect('key_press_event', on_keypress)
+            plt.show()
+
+        def animation_data_gen():
+            for step, _ in enumerate(self.trajectory):
+                yield step
+
+        def update_plot(step):
+            if self.manual_control:
+                return animated_elements
+            step = slider.val + 1
+            if step >= len(self.trajectory):
+                step = len(self.trajectory) - 1
+                slider.set_val(step)
+                return lines
+            slider.set_val(step)
+            self.manual_control = False
+            return animated_elements
+
+        def update_slider(step):
+            step = math.floor(step)
+            self.current_frame = step
+            self.manual_control = True
+            render_frame(step)
+
+        def render_frame(step):
+            frame = self.trajectory.obs[step]["pov"]
+            l2.set_xdata(step)
+            l3.set_xdata(step)
+            action_name = ActionSpace.action_name(self.trajectory.actions[step])
+            txt_action.set_text(f'Action: {action_name}')
+            img.set_array(frame)
+            fig.canvas.flush_events()
+            fig.canvas.draw_idle()
+
+        def on_click(event):
+            (sxm, sym), (sxM, syM) = slider.label.clipbox.get_points()
+            if sxm < event.x < sxM and sym < event.y < syM:
+                return
+            self.manual_control = False if self.manual_control else True
+
+        def on_keypress(event):
+            if event.key == 'enter':
+                plt.close('all')
+            if len(self.trajectory) < 2:
+                return
+            if event.key == ' ':
+                self.manual_control = False if self.manual_control else True
+            else:
+                self.manual_control = True
+                if event.key == 'right':
+                    if self.current_frame < len(self.trajectory) - 1:
+                        self.current_frame += 1
+                elif event.key == 'left':
+                    if self.current_frame > 0:
+                        self.current_frame -= 1
+                slider.set_val(self.current_frame)
+
+        fig = plt.figure(figsize=(9, 8))
+        ax_pov = plt.subplot2grid((9, 8), (0, 0), colspan=5, rowspan=5)
+        img = ax_pov.imshow(self.trajectory.obs[self.current_frame]["pov"], animated=True)
+        txt_action = ax4.text(.1, 1.35, 'Action:')
+        animated_elements = [img, txt_action]
+
+        if len(self.trajectory) > 0:
+            ax_steps = plt.axes([0.2, 0.15, 0.65, 0.03])
+            slider = Slider(ax_steps, 'Step', 0, len(self.trajectory) - 1, valinit=0)
+
+        play()
