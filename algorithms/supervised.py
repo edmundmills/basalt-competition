717cf027ffd456649e5fac5bf3b2a33f11ab590e
Edmund Mills <edmund.martin.mills@gmail.com>
2021-09-26 07:22:29 -0700

    Add bc with train/test set

--- algorithms/supervised.py
+++ algorithms/supervised.py
@@ -10,19 +10,13 @@ from torch.utils.data import DataLoader
 
 
 class SupervisedLearning(Algorithm):
-    def __init__(self, train_dataset, model, config, test_dataset=None):
+    def __init__(self, config):
         super().__init__(config)
-        self.epochs = config['epochs']
-        self.lr = config['learning_rate']
-        self.batch_size = config['batch_size']
-        self.model = model
-        self.train_dataset = train_dataset
-        self.test_dataset = test_dataset
+        self.epochs = config.epochs
+        self.lr = config.learning_rate
+        self.batch_size = config.batch_size
 
-    def __call__(self, _env=None):
-        model = self.model
-        train_dataset = self.train_dataset
-        test_dataset = self.test_dataset
+    def __call__(self, model, train_dataset, test_dataset=None, _env=None):
         optimizer = th.optim.Adam(model.parameters(), lr=self.lr)
         train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size,
                                       shuffle=True, num_workers=4)
@@ -39,17 +33,18 @@ class SupervisedLearning(Algorithm):
                 loss.backward()
                 optimizer.step()
 
+                if self.wandb:
+                    wandb.log({'train_loss': loss.detach()}, step=self.iter_count)
+
                 iter_count += 1
                 self.log_step()
 
             print(f'Epoch #{epoch + 1} completed')
-            if self.test_dataset is not None and self.wandb:
+            if test_dataset is not None and self.wandb:
                 test_batch = test_dataset.sample()
                 eval_metrics = self.eval(model, test_batch)
                 print('Metrics: ', eval_metrics)
-                wandb.log(
-                    {**eval_metrics,
-                     'average_its_per_s': self.iteration_rate()})
+                wandb.log(eval_metrics)
 
         print('Training complete')
         model.save(os.path.join('train', f'{self.name}.pth'))
--- bc_experiment.py
+++ bc_experiment.py
@@ -1,3 +1,5 @@
+from omegaconf import DictConfig, OmegaConf
+from hydra import compose, initialize
 from helpers.datasets import TrajectoryStepDataset
 from algorithms.supervised import SupervisedLearning
 from networks.bc import BC
@@ -20,30 +22,34 @@ import coloredlogs
 coloredlogs.install(logging.DEBUG)
 
 
-def main():
-    environment = 'MineRLBasaltBuildVillageHouse-v0'
-    os.environ['MINERL_ENVIRONMENT'] = environment
+def get_config(args):
+    with initialize(config_path='conf'):
+        cfg = compose('config.yaml', overrides=args.overrides)
 
+    cfg.device = "cuda:0" if th.cuda.is_available() else "cpu"
+    cfg.wandb = args.wandb
+    cfg.method.wandb = args.wandb
+    cfg.hydra_base_dir = os.getcwd()
+    print(OmegaConf.to_yaml(cfg))
+    return cfg
+
+
+def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--debug-env', dest='debug_env',
                            action='store_true', default=False)
-    argparser.add_argument('--wandb', dest='wandb',
-                           action='store_true', default=False)
+    argparser.add_argument('--wandb-false', dest='wandb',
+                           action='store_false', default=True)
+    argparser.add_argument("overrides", nargs="*", default=["env=cave", "method=bc"])
 
     args = argparser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
     logging.getLogger().setLevel(logging.INFO)
 
-    config = dict(
-        learning_rate=.001,
-        epochs=5,
-        batch_size=64,
-        n_observation_frames=3,
-        environment=environment,
-        algorithm='supervised_learning',
-        wandb=args.wandb
-    )
+    config = get_config(args)
+    environment = config.env.name
+    os.environ['MINERL_ENVIRONMENT'] = environment
 
     # Start WandB
     if args.wandb:
@@ -54,18 +60,20 @@ def main():
         )
 
     # Train Agent
-    training_algorithm = SupervisedLearning(config)
-    model = BC(n_observation_frames=config['n_observation_frames'])
+    training_algorithm = SupervisedLearning(config.method)
+    model = BC(n_observation_frames=config.n_observation_frames)
     # set up dataset
     dataset = TrajectoryStepDataset(
-        debug_dataset=args.debug_env, n_observation_frames=config['n_observation_frames'])
+        debug_dataset=args.debug_env, n_observation_frames=config.n_observation_frames)
     train_dataset_size = int(0.8 * len(dataset))
     train_dataset, test_dataset \
         = th.utils.data.random_split(dataset,
                                      [train_dataset_size, len(
                                          dataset) - train_dataset_size],
                                      generator=th.Generator().manual_seed(42))
-
+    print('Train dataset size: ', len(train_dataset))
+    print('Test dataset size: ', len(test_dataset))
+    print('Training agent...')
     training_algorithm(model,
                        train_dataset,
                        test_dataset=test_dataset)
