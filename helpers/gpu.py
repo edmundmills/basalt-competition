5c3adff8071e2320d129c8ef6a6dba00880ee7f5
Edmund Mills <edmund.martin.mills@gmail.com>
2021-09-28 16:21:11 -0700

    Small performance optimization - don't cat before to(gpu)

--- helpers/gpu.py
+++ helpers/gpu.py
@@ -4,12 +4,18 @@ import torch as th
 
 
 def states_to_device(tuple_of_states, device):
-    state_lengths = [states[0].size()[0] for states in tuple_of_states]
-    all_states = [th.cat(state_component, dim=0).to(device) for state_component
-                  in zip(*tuple_of_states)]
-    list_of_states = zip(*[th.split(state_component, state_lengths, dim=0)
-                           for state_component in all_states])
-    return tuple(list_of_states)
+    # # this is slower, but may be better for larger batch sizes?
+    # state_lengths = [states[0].size()[0] for states in tuple_of_states]
+    # all_states = [th.cat(state_component, dim=0).to(device) for state_component
+    #               in zip(*tuple_of_states)]
+    # list_of_states = zip(*[th.split(state_component, state_lengths, dim=0)
+    #                        for state_component in all_states])
+    # return tuple(list_of_states)
+    states = []
+    for state in tuple_of_states:
+        state = [state_component.to(device) for state_component in state]
+        states.append(state)
+    return tuple(states)
 
 
 def cat_states(tuple_of_states):
