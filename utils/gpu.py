150687958948ac77a9b51c226e860276f36f583f
Edmund Mills <edmund.martin.mills@gmail.com>
2021-10-10 08:51:40 -0700

    Add normalize obs option

--- utils/gpu.py
+++ utils/gpu.py
@@ -33,11 +33,60 @@ class GPULoader:
         ones = th.ones(starting_count.size())
         self.item_normalization = th.cat((starting_count, ones), dim=1).to(self.device)
         self.loading_sequences = config.lstm_layers > 0
+        self.normalize_obs = config.normalize_obs
+        self.env = config.env.name
+        self.mobilenet_normalization = (
+            th.FloatTensor([0.485, 0.456, 0.406]).to(self.device).reshape(
+                3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+            th.FloatTensor([0.229, 0.224, 0.225]).to(self.device).reshape(
+                3, 1, 1).tile((config.n_observation_frames, 1, 1)))
+        all_pov_normalization_factors = {
+            'MineRLBasaltBuildVillageHouse-v0': (
+                th.FloatTensor([109.01, 108.78, 95.13]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([50.83, 56.32, 77.89]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+            'MineRLBasaltCreateVillageAnimalPen-v0': (
+                th.FloatTensor([107.61, 125.33, 112.16]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([43.69, 50.70, 93.10]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+            'MineRLBasaltFindCave-v0': (
+                th.FloatTensor([106.44, 127.52, 126.61]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([45.06, 54.25, 97.68]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+            'MineRLBasaltMakeWaterfall-v0': (
+                th.FloatTensor([109.11, 117.04, 131.58]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([51.78, 60.46, 85.87]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+            'MineRLNavigateExtremeDense-v0': (
+                th.FloatTensor([70.69, 71.73, 88.11]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([43.07, 49.05, 72.84]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+            'MineRLNavigateDense-v0': (
+                th.FloatTensor([66.89, 76.77, 104.20]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1)),
+                th.FloatTensor([54.26, 60.83, 88.11]).to(self.device).reshape(
+                    3, 1, 1).tile((config.n_observation_frames, 1, 1))),
+        }
+        self.pov_normalization = all_pov_normalization_factors[self.env]
 
     def normalize_state(self, state):
         state = list(state)
-        state[0] /= 255.0
-        state[1] /= self.item_normalization
+        if self.normalize_obs:
+            state[0] = (state[0] - self.pov_normalization[0])/self.pov_normalization[1]
+            state[0] = state[0] * self.mobilenet_normalization[1] \
+                + self.mobilenet_normalization[0]
+            state[1] /= self.item_normalization
+            if self.env not in \
+                    ['MineRLNavigateDense-v0', 'MineRLNavigateExtremeDense-v0']:
+                state[1] -= 0.5
+        else:
+            state[0] /= 255.0
+            state[1] /= self.item_normalization
         return tuple(state)
 
     def states_from_sequence(self, sequence):
